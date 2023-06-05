66d03efb430cd309fe2b87fdf541246098fee87b
Leona07 <leona.varazdin@gmail.com>
2023-06-05 12:46:39 +0200

    new

--- src/index.js
+++
@@ -1,7 +0,0 @@
-
-import { collectButton } from "../../package/src/js";
-import create_input_table from '../../package/src/js';
-console.log("Initialized My data");
-create_input_table('table', 'tableButtons', 1);
-collectButton();
-
